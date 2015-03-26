29a28fd97cbc1615fbda7b4793f319a27b13b8f8
尹建波 <jianbo.yin@alibaba-inc.com>
2015-03-26 12:07:41 +0800

    第一次提交

---
+++ src/main/java/autonavi/com/autonavi/App.java
@@ -0,0 +1,13 @@
+package autonavi.com.autonavi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
