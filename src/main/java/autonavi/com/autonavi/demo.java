523795dacd5a661fb148cc0dcf0fc2dfb7665046
尹建波 <jianbo.yin@alibaba-inc.com>
2015-03-31 17:25:41 +0800

    demo add frist

---
+++ src/main/java/autonavi/com/autonavi/demo.java
@@ -0,0 +1,15 @@
+package autonavi.com.autonavi;
+
+/**
+ * 
+ * @author jianbo.yin(秋波)
+ *
+ * @version 创建时间：2015年3月31日 下午5:25:01
+ */
+public class demo {
+	public static void main(String[] args) {
+		
+		System.out.println("he he~~~~~~");
+		
+	}
+}
